b4713aa53cb07f49304aeac7f6a70a9507f8b4fc
skmta <a07090072@gmail.com>
2019-05-01 21:22:21 +0900

    編集中

--- app/src/main/java/com/example/a100thingstodo/MainActivity.java
+++ app/src/main/java/com/example/a100thingstodo/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.a100thingstodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager mViewPager = (ViewPager)findViewById(R.id.viewpager);
+        PagerAdapter mPagerAdapter = new MyPagerAdapter();
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+    private class MyPagerAdapter extends PagerAdapter {
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            // レイアウトファイル名を配列で
+            //100まで
+            int[] pages = {R.layout.page1, R.layout.page2, R.layout.page3};
+
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View layout ;
+            layout = inflater.inflate(pages[position], null);
+            ((ViewPager) container).addView(layout);
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager)container).removeView((View)object);
+        }
+
+        @Override
+        public int getCount() {
+            // ページ数を返す
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view.equals(object);
+        }
     }
 }
